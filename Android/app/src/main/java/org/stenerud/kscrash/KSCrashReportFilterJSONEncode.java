9a94926ac9c94e0c9bbcdf527d208a5490628246
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-11-13 17:50:39 +0800

    Jack: 将对象转换为Json字符串工具类

--- Android/app/src/main/java/org/stenerud/kscrash/KSCrashReportFilterJSONEncode.java
+++ Android/app/src/main/java/org/stenerud/kscrash/KSCrashReportFilterJSONEncode.java
@@ -1,25 +1,3 @@
-//
-//  Copyright (c) 2017 Karl Stenerud. All rights reserved.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall remain in place
-// in this source code.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-//
-
 package org.stenerud.kscrash;
 
 import org.json.JSONObject;
@@ -27,14 +5,19 @@ import org.json.JSONObject;
 import java.util.LinkedList;
 import java.util.List;
 
+
 /**
+ * 将对象转换为Json字符串工具类
  * Encodes objects into a JSON encoded string.
- *
+ * <p>
  * Input: JSON encodable object
  * Output: String (JSON encoded)
  */
 public class KSCrashReportFilterJSONEncode implements KSCrashReportFilter {
+
     private int indentDepth = -1;
+
+
 
     /**
      * Constructor.
@@ -54,20 +37,21 @@ public class KSCrashReportFilterJSONEncode implements KSCrashReportFilter {
     }
 
     @Override
-    public void filterReports(List reports, CompletionCallback completionCallback) throws KSCrashReportFilteringFailedException {
+    public void filterReports(List reports, CompletionCallback completionCallback)
+            throws KSCrashReportFilteringFailedException {
         List<String> processedReports = new LinkedList<String>();
         try {
             for (Object report : reports) {
-                JSONObject object = (JSONObject)report;
-                String string = null;
-                if(indentDepth >= 0) {
+                JSONObject object = (JSONObject) report;
+                String string;
+                if (indentDepth >= 0) {
                     string = object.toString(indentDepth);
                 } else {
                     string = object.toString();
                 }
                 processedReports.add(string);
             }
-        } catch(Throwable error) {
+        } catch (Throwable error) {
             throw new KSCrashReportFilteringFailedException(error, reports);
         }
         completionCallback.onCompletion(processedReports);
