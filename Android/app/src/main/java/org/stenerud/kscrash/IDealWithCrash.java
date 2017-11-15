bf1fb1c250997cc874f8354b871e242617757677
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-11-15 19:18:47 +0800

    Jack: 增加用户自定义处理crash的接口

---
+++ Android/app/src/main/java/org/stenerud/kscrash/IDealWithCrash.java
@@ -0,0 +1,16 @@
+package org.stenerud.kscrash;
+
+
+/**
+ * 用户自定义处理crash的接口
+ */
+public interface IDealWithCrash{
+
+    /**
+     * 处理crash日志的回调接口
+     * @param summary 异常的精简信息
+     * @param detail  异常的详细信息
+     */
+    void dealWithCrash(Throwable summary, String detail);
+}
+
