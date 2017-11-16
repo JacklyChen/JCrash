aaef6e3ad46ee2a7eb756f0fe339f6515d8a6b1d
xuning108986@pwrd.com <xuning108986@pwrd.com>
2017-11-16 21:55:20 +0800

    Jack: 新增公共常量

---
+++ Android/app/src/main/java/org/stenerud/kscrash/common/Constant.java
@@ -0,0 +1,41 @@
+package org.stenerud.kscrash.common;
+
+
+
+
+/**
+ * 公共常量
+ * 完美世界
+ * @Author Jack
+ * @Date 2017/11/16 17:45
+ * @Copyright:wanmei.com Inc. All rights reserved.
+ */
+public class Constant {
+
+
+    public static final String SDK_VERSION = "1.0.0";
+
+
+    /**
+     * 系统类型  1：ios越狱，2：安卓，3：ios官方
+     */
+    public static final short OS_TYPE = 2;
+
+    /**
+     * 默认值
+     */
+    public static final class DefaultValue{
+        /**
+         * 无默认值
+         */
+        public static final String NO_DEFAULT = "";
+        /**
+         * NULL字符串
+         */
+        public static final String NULL = "NULL";
+        /**
+         * JsonArray或字符串数组的默认值
+         */
+        public static final String ARRAY_DEFAULT = "[]";
+    }
+}
